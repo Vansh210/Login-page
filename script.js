05fa269e483e3f9bc50ecdaa5883c1f805ba48df
Vansh210 <26vanshbarapatre@gmail.com>
2024-09-26 17:45:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,5 @@ document.getElementById('optinForm').addEventListener('submit', function(event) 
     const fullname = document.getElementById('fullname').value;
     const email = document.getElementById('email').value;
     console.log(`Full Name: ${fullname}\nEmail: ${email}`);
-    alert(`Thank you for subscribing, ${fullname}!`);
     window.location.href = 'https://sites.google.com/view/mindful-connect/thank-you';
 });
